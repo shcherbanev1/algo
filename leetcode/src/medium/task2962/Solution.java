997a7422e1d37b7f7f92a297d37efea31fae3faf
shcherbanev <shcherbanev1@yandex.ru>
2025-05-06 02:06:53 +0300

    solve task 2962

---
+++ leetcode/src/medium/task2962/Solution.java
@@ -0,0 +1,45 @@
+package medium.task2962;
+
+class Solution {
+
+    public static long countSubarrays(int[] nums, int k) {
+        long ans = 0;
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (max < nums[i]) {
+                max = nums[i];
+            }
+        }
+
+        int point = 0;
+        int endMax = 0;
+        int startMax = 0;
+        int maxCount = 0;
+        while (endMax < nums.length) {
+            while (endMax < nums.length && maxCount != k) {
+                if (nums[endMax] == max) {
+                    maxCount++;
+                }
+                endMax++;
+            }
+            endMax--;
+            while (startMax < nums.length && nums[startMax] != max) {
+                startMax++;
+            }
+            if (maxCount == k) {
+                ans += (long) (startMax + 1 - point) * (nums.length - endMax);
+                point = startMax + 1;
+            }
+            startMax++;
+            endMax++;
+            maxCount--;
+        }
+        System.out.println(ans);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        countSubarrays(new int[]{1, 4, 2, 1}, 3); // 11
+    }
+
+}
